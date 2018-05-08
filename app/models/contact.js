712ea1d9a68fa066c4f90ea43a5afa30871657dc
Melissa Saldana <melissasaldana@Melissas-MacBook-Air.local>
2018-05-07 20:18:12 -0400

    push

--- app/models/contact.js
+++ app/models/contact.js
@@ -5,13 +5,13 @@ var Schema = mongoose.Schema;
 var ContactSchema = new Schema ({
     companyName: {type: String, lowercase: true, required: true},
     contactName: {type: String,lowercase: true, required: true},
-    streetAddress: {type: String, lowercase: true},
-    city: {type: String,required: true},
-    state: {type: String,lowercase: true},
-    zip: {type: String,lowercase: true},
-    phone: {type: String,lowercase: true},
-    email: {type: String, lowercase: true},
-    message: {type: String,lowercase: true}
+    streetAddress: {type: String, lowercase: true, required: true},
+    city: {type: String, lowercase: true, required: true},
+    state: {type: String, lowercase: true, required: true},
+    zip: {type: String, lowercase: true, required: true},
+    phone: {type: String, lowercase: true, required: true},
+    email: {type: String, lowercase: true, required: true},
+    message: {type: String, lowercase: true, required: true}
 });
 //exports to server
 module.exports = mongoose.model('Contact', ContactSchema);
