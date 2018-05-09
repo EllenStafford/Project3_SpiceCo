e21a83a4527b333c49220ab586f9ca2815176f83
Melissa Saldana <melissasaldana@Melissas-MacBook-Air.local>
2018-05-08 20:25:50 -0400

    half way to messages

--- public/app/controllers/requestsController.js
+++ public/app/controllers/requestsController.js
@@ -1,5 +1,18 @@
 angular.module("requestsController", [])
 
-.controller("reqCtrl", function(){
-    console.log("test");
+.controller("reqCtrl", function(Contact){
+
+    var app = this;
+    app.accessDeny = true;
+    app.erroMg = false;
+
+    Contact.getRequests().then(function(data){
+        if(data.data.success){
+
+        }else{
+            app.erroMg = data.data.message;
+        }
+        console.log(data)
+    });
+    
 });
